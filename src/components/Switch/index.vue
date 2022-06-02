7d494d496aac52ccf646517bf29abfdb9ce47138
peakchao <zhangzhichaolove@vip.qq.com>
2022-06-02 16:34:16 +0800

    fix build error.

--- src/components/Switch/index.vue
+++ src/components/Switch/index.vue
@@ -1,19 +1,3 @@
-<template>
-    <button class="ds-switch" :class="checked ? 'ds-switch-checked' : ''" @click="switchHandel" :style="{
-      'background-color': checked ? '#338eff' : '#00000040',
-      transform:
-        size === 'small'
-          ? 'scale(.7, .7)'
-          : size === 'large'
-          ? 'scale(1.3, 1.3)'
-          : 'scale(1, 1)',
-    }">
-        <div class="ds-switch-hendel"></div>
-        <span class="ds-switch-text" :class="checked ? 'ds-switch-text-checked' : 'ds-switch-text-unchecked'"
-            v-if="checkedText && uncheckedText">{{ checked ? checkedText : uncheckedText }}</span>
-    </button>
-</template>
-
 <script setup lang='ts'>
 import { ref } from "vue";
 
@@ -35,13 +19,25 @@ const checked = ref<boolean>(false);
 const switchHandel = () => {
     checked.value = !checked.value;
     emits("switch", checked.value);
-    // console.log(checked.value);
 };
 </script>
+<template>
+    <button class="ds-switch" :class="checked ? 'ds-switch-checked' : ''" @click="switchHandel" :style="{
+        'background-color': checked ? '#338eff' : '#00000040',
+        transform:
+            size === 'small'
+                ? 'scale(.7, .7)'
+                : size === 'large'
+                    ? 'scale(1.3, 1.3)'
+                    : 'scale(1, 1)',
+    }">
+        <div class="ds-switch-hendel"></div>
+        <span class="ds-switch-text" :class="checked ? 'ds-switch-text-checked' : 'ds-switch-text-unchecked'"
+            v-if="checkedText && uncheckedText">{{ checked ? checkedText : uncheckedText }}</span>
+    </button>
+</template>
 
 <style scoped lang='css'>
-@default-color: color('#338eff');
-
 .ds-switch {
     position: relative;
     display: inline-flex;
@@ -59,6 +55,7 @@ const switchHandel = () => {
     box-shadow: 0 0 0 2px #00000020;
     overflow: hidden;
 }
+
 .ds-switch-hendel {
     position: absolute;
     top: 2px;
@@ -69,18 +66,22 @@ const switchHandel = () => {
     background-color: #fff;
     transition: all 0.2s ease-in-out;
 }
+
 .ds-switch-checked .ds-switch-hendel {
     left: calc(100% - 20px);
 }
+
 .ds-switch-text {
     color: #fff;
     font-size: 12px;
     position: absolute;
     font-weight: 600;
 }
+
 .ds-switch-text-checked {
     left: 7px;
 }
+
 .ds-switch-text-unchecked {
     right: 7px;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      components: resolve(__dirname, 'src/components'),
+      utils: resolve(__dirname, 'src/utils'),
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.ts'),
+      name: 'light-player',
+      fileName: (format) => `light-player.${format}.ts`
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 9191,
+    open: true
+  }
 })
