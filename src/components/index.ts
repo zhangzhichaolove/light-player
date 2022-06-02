02c9fe98dd9bedebd61bc3eb4a9396f8fa740209
zhangzhichao <zhangzhichaolove@vip.qq.com>
2022-06-02 22:13:17 +0800

    add dts config.

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,14 +4,16 @@ import Switch from './Switch'
 //按需引入
 const components = [Switch]
 
-//全局引入
+//全局引入, 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
 const install = (app: App) => {
+    // 遍历注册全局组件
     components.map(item => {
         app.component(item.name, item)
     })
 }
 
 export default {
+    // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
     install,
     ...components
 }
